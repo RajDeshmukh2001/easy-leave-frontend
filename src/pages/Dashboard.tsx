3b48187a4d07bdde040850b2fee306be544777c5
shantanuk7 <shantanukulkarni365@gmail.com>
2026-03-26 17:24:43 +0530

    refactor(LMS-53): remove Dashboard page not required for sidebar implementation

--- src/pages/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-const Dashboard = () : React.JSX.Element => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
-
-export default Dashboard
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,12 +1,11 @@
 import Layout from '@/components/Layout'
-import Dashboard from '@/pages/Dashboard'
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 const AppRoutes = () : React.JSX.Element => {
   return (
     <Routes>
         <Route element={<Layout/>}>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/#"/>
         </Route>
     </Routes>
   )
