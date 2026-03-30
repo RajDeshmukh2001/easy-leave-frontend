d8056d8340dd188310e2918f89424cba471d1823
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-03-30 17:33:40 +0530

    feat: add a new Dashboard route

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,6 +1,11 @@
-import { CalendarPlus } from 'lucide-react';
+import { CalendarPlus, LayoutDashboard } from 'lucide-react';
 import type { NavItem } from '../types/navigation';
 export const EMPLOYEE_NAV_ITEMS: NavItem[] = [
+    {
+      name: "Dashboard",
+      icon: LayoutDashboard,
+      href: "/dashboard",
+    },
     {
       name: "Leave",
       icon: CalendarPlus,
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Layout from '@/components/Layout';
 import Leave from '@/pages/Leave';
-
+import Dashboard from '@/pages/Dashboard';
 const AppRoutes = (): React.JSX.Element => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path='/' element={<div>Home page</div>} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/leave' element={<Leave />} />
       </Route>
     </Routes>
   )
 }
 
-export default AppRoutes
+export default AppRoutes;
