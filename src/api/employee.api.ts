299edfcd4b5267dec7383afa85a30edec1a5fb58
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-06 16:29:13 +0530

    feat(lms-19): update the getEmployees function to get correct data from backend

--- src/api/employee.api.ts
+++ src/api/employee.api.ts
@@ -6,11 +6,17 @@ type EmployeeParams = {
   size?: number;
 };
 
-export const getEmployees = async ( params: EmployeeParams ): Promise<UserResponse[]> => {
+export type EmployeeApiResponse = {
+  content: UserResponse[];
+  last: boolean;
+  totalPages: number;
+  number: number;
+};
+
+export const getEmployees = async ( params: EmployeeParams ): Promise<EmployeeApiResponse> => {
   const { data } = await axiosInstance.get('/api/users', { params });
 
   if (!data.success) {
-    console.error('Error fetching employees:', data.message);
     throw new Error(data.message || 'Failed to fetch employees');
   }
 
