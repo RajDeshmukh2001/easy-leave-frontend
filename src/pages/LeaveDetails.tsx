20a86ec1b0570566198e73c663e35cb9c8d2b7fb
RajDeshmukh2001 <rajdd61@gmail.com>
2026-04-09 22:38:45 +0530

    feat(LMS-27): use LeaveForm in LeaveDetails page with pre filled fields

--- src/pages/LeaveDetails.tsx
+++ src/pages/LeaveDetails.tsx
@@ -1,6 +1,9 @@
 import { fetchLeaveById } from '@/api/leave.api';
+import LeaveForm from '@/components/LeaveForm';
 import Loading from '@/components/Loading';
 import PageHeader from '@/components/PageHeader';
+import useLeaveCategories from '@/hooks/useLeaveCategories';
+import type { LeaveFormValues } from '@/types/leaveForm';
 import type { LeaveResponse } from '@/types/leaves';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
@@ -10,6 +13,7 @@ const LeaveDetails = (): React.JSX.Element => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const { id } = useParams();
+  const { categories } = useLeaveCategories();
 
   const fetchLeaveDetails = async (id: string | undefined) => {
     try {
@@ -31,6 +35,10 @@ const LeaveDetails = (): React.JSX.Element => {
     fetchLeaveDetails(id);
   }, [id]);
 
+  const handleUpdateLeave = async (values: LeaveFormValues) => {
+    console.log(values);
+  };
+
   if (loading) {
     return <Loading />;
   }
@@ -43,9 +51,27 @@ const LeaveDetails = (): React.JSX.Element => {
     );
   }
 
+  const matchedCategory = categories.find((category) => category.name === leave.type);
+
+  const updateLeaveInitialValues: LeaveFormValues = {
+    leaveCategoryId: matchedCategory?.id || '',
+    dateRange: { from: new Date(leave.date), to: new Date(leave.date) },
+    duration: leave.duration,
+    startTime: leave.startTime,
+    description: leave.reason ?? '',
+  };
+
   return (
     <div className="w-full p-3">
       <PageHeader pageTitle="Leave Details" pageSubtitle="View and manage your leave details" />
+
+      <div className="max-w-1/2 flex bg-white rounded-2xl shadow-xs border border-neutral-200 h-full">
+        <LeaveForm
+          initialValues={updateLeaveInitialValues}
+          onSubmit={handleUpdateLeave}
+          submitLabel="Update Leave"
+        />
+      </div>
     </div>
   );
 };
