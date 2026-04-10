1d40c3efd3bb147d35c4949a0e34d8b825a354a1
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-10 15:52:00 +0530

    style(lms): update the bg of header of table

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -38,13 +38,13 @@ function Dashboard(): React.JSX.Element {
   ];
 
   return (
-    <div className="w-full h-screen flex flex-col p-3">
+    <div className="w-full h-screen flex flex-col p-4">
       <PageHeader
         pageTitle="Dashboard"
         pageSubtitle={`Welcome ${user?.name}! Here you can find an overview of your Leaves`}
       />
       <div className="flex flex-col min-h-0 w-full mb-5 rounded-2xl shadow-xs border border-neutral-200">
-        <div className="bg-sidebar rounded-t-2xl ">
+        <div className="bg-sidebar/98 py-2 px-1 rounded-t-2xl ">
           <h1 className="text-2xl text-sidebar-foreground font-bold mb-4 px-4 py-2">
             Upcoming Leaves
           </h1>
