e483d98881ee7a9d2b25f71976cf2cbdb255eb7d
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-16 15:07:48 +0530

    feat(LMS-69): Add logout API call

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -6,7 +6,16 @@ export const getAuthenticatedUser = async (): Promise<User> => {
   const { data } = await axiosInstance.get<ApiResponse<User>>(`/api/auth/me`);
   if (!data.success) {
     console.error('Error fetching current user:', data.message);
+
     throw new Error(data.message || 'Failed to fetch current user');
   }
   return data.data;
+};
+
+export const logout = async (): Promise<void> => {
+  const { data } = await axiosInstance.post<ApiResponse<void>>('/api/auth/logout');
+  if (!data.success) {
+    console.error('Error logging out:', data.message);
+    throw new Error(data.message || 'Logout failed');
+  }
 };
