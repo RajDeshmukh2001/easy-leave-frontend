3458ad9b219217201d596c3cec77500e45eb9bf0
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-22 23:54:18 +0530

    refactor(LMS-81): rename leave page to LeaveAndRequest

---
+++ src/pages/LeaveAndRequest.tsx
@@ -0,0 +1,36 @@
+import LeaveTab from '@/components/leave/LeaveTab';
+import PageHeader from '@/components/PageHeader';
+import RequestTab from '@/components/request/RequestTab';
+import { useState } from 'react';
+
+function LeaveAndRequest(): React.JSX.Element {
+  const [activeTab, setActiveTab] = useState<'leave' | 'request'>('leave');
+
+  return (
+    <div className="w-full md:h-screen flex flex-col p-4">
+      <PageHeader pageTitle="Leaves" pageSubtitle="Submit a new leave or raise a request" />
+
+      <div className="flex w-fit rounded-lg border border-neutral-200 bg-white mt-2">
+        <button
+          onClick={() => setActiveTab('leave')}
+          className={`px-6 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer
+            ${activeTab === 'leave' ? 'bg-(--technogise-blue) text-white' : 'text-gray-600'}`}
+        >
+          Leave
+        </button>
+        <button
+          onClick={() => setActiveTab('request')}
+          className={`px-6 py-2 rounded-lg text-sm font-medium transition-colors cursor-pointer
+            ${activeTab === 'request' ? 'bg-(--technogise-blue) text-white' : 'text-gray-600'}`}
+        >
+          Raise Request
+        </button>
+      </div>
+      <div className="flex justify-between gap-5">
+        {activeTab === 'leave' ? <LeaveTab /> : <RequestTab />}
+      </div>
+    </div>
+  );
+}
+
+export default LeaveAndRequest;
