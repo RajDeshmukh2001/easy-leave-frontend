a6989bec648f701dafd683ec62b69d55ca55e6a9
shantanuk7 <shantanukulkarni365@gmail.com>
2026-04-23 11:49:15 +0530

    fix(LMS-30): fix the scrolling table header in holidays page

--- src/pages/Holidays.tsx
+++ src/pages/Holidays.tsx
@@ -126,7 +126,7 @@ const Holidays = (): React.JSX.Element => {
           </Formik>
         </div>
         <div className="flex flex-1 flex-col rounded-2xl mb-5 max-h-150 md:max-h-screen shadow-xs border border-neutral-200">
-          <div className="flex items-center p-3 justify-between bg-sidebar/98 rounded-t-2xl ">
+          <div className="flex items-center p-3 justify-between bg-sidebar/98 rounded-t-2xl">
             <h1 className="text-xl md:text-2xl text-sidebar-foreground font-bold px-3 py-2">
               All Holidays
             </h1>
@@ -136,18 +136,16 @@ const Holidays = (): React.JSX.Element => {
               onChange={(val) => setHolidayType(val as HolidayListOptions)}
             />
           </div>
-          <div className="flex-1 lg:overflow-y-auto">
-            {loading && <Loading />}
-            {error && <p className="p-3 text-red-700">{error}</p>}
-            {!loading && !error && (
-              <Table
-                data={holidays}
-                columns={holidayTableColumns}
-                message="No holidays found."
-                getRowKey={(holiday: HolidayResponse) => holiday.id}
-              />
-            )}
-          </div>
+          {loading && <Loading />}
+          {error && <p className="p-3 text-red-700">{error}</p>}
+          {!loading && !error && (
+            <Table
+              data={holidays}
+              columns={holidayTableColumns}
+              message="No holidays found."
+              getRowKey={(holiday: HolidayResponse) => holiday.id}
+            />
+          )}
         </div>
       </div>
     </div>
