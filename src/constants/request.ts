2fa0bc8db50febea4f0d2cf856474817f56dc22f
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-24 14:34:22 +0530

    feat(LMS-37): add constant file for status and type

--- src/constants/request.ts
+++ src/constants/request.ts
@@ -2,3 +2,4 @@ export type RequestScope = 'SELF' | 'ORGANIZATION';
 export type RequestStatus = 'PENDING' | 'APPROVED' | 'REJECTED' | 'ALL';
 export type RequestType = 'PAST_LEAVE' | 'COMPENSATORY_OFF' | 'ALL';
 export const REQUEST_STATUS_OPTIONS: RequestStatus[] = ['ALL', 'PENDING', 'APPROVED', 'REJECTED'];
+
