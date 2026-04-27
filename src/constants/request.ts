a77ffc3f5ef5ab4d27469dd1c48f2bdd8b87b363
Rakshit Saxena <rakshitsaxena96@gmail.com>
2026-04-27 14:38:20 +0530

    docs(LMS-37):update readme file

--- src/constants/request.ts
+++ src/constants/request.ts
@@ -2,4 +2,3 @@ export type RequestScope = 'SELF' | 'ORGANIZATION';
 export type RequestStatus = 'PENDING' | 'APPROVED' | 'REJECTED' | 'ALL';
 export type RequestType = 'PAST_LEAVE' | 'COMPENSATORY_OFF' | 'ALL';
 export const REQUEST_STATUS_OPTIONS: RequestStatus[] = ['ALL', 'PENDING', 'APPROVED', 'REJECTED'];
-
