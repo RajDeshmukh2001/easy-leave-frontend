711044d9e26f562d2cb4bb12571c92677aca2f9d
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-04-29 14:54:30 +0530

    refactor(lms-92): correct the style of header and dropdown in TableHeader

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -7,7 +7,7 @@ type TableHeaderProps = {
 
 function TableHeader({ title, dropDownFilter }: TableHeaderProps): JSX.Element {
   return (
-    <div className="py-2 px-1 bg-white rounded-t-2xl">
+    <div className="py-2 px-1 flex justify-between w-full items-center bg-white rounded-t-2xl">
       <h1 className="text-xl md:text-xl text-gray-800 font-bold px-4 py-2">{title}</h1>
       {dropDownFilter}
     </div>
